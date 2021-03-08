8c69b56e4652780fd097df8526667e16e0f6a212
franklindyer <franklin@dyer.me>
2021-03-08 10:38:43 -0700

    cleaned up polyhedra files

--- polyhedra/loader-1.0.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import itertools
-
-exec(open("polyhedra-1.0/stl-Tools.py").read())
-exec(open("polyhedra-1.0/stl-Solid.py").read())
-exec(open("polyhedra-1.0/stl-Convex.py").read())
-exec(open("polyhedra-1.0/stl-Common.py").read())
-exec(open("polyhedra-1.0/stl-Uniform.py").read())
--- polyhedra/loader-2.0.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import itertools
-
-exec(open("polyhedra-2.0/tools.py").read())
-exec(open("polyhedra-2.0/Solid.py").read())
-exec(open("polyhedra-2.0/ConvexSolid.py").read())
-exec(open("polyhedra-2.0/PlatonicSolid.py").read())
-exec(open("polyhedra-2.0/ArchimedeanSolid.py").read())
