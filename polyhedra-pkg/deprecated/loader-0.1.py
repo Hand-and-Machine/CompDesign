ae7ad0974a79164a30019966a602f25092b34335
franklindyer <franklin@dyer.me>
2021-04-18 15:11:01 -0600

    removed polyhedra stuff

--- polyhedra-pkg/deprecated/loader-0.1.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import itertools
-
-exec(open("polyhedra-0.1/stl-Tools.py").read())
-exec(open("polyhedra-0.1/stl-Solid.py").read())
-exec(open("polyhedra-0.1/stl-Convex.py").read())
-exec(open("polyhedra-0.1/stl-Common.py").read())
-exec(open("polyhedra-0.1/stl-Uniform.py").read())
--- polyhedra/loader-0.2.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import itertools
-
-exec(open("polyhedra-0.2/tools.py").read())
-exec(open("polyhedra-0.2/Solid.py").read())
-exec(open("polyhedra-0.2/ConvexSolid.py").read())
-exec(open("polyhedra-0.2/PlatonicSolid.py").read())
-exec(open("polyhedra-0.2/ArchimedeanSolid.py").read())
